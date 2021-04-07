10caaa1f0456caee9de1643bc4ab3764d596a9a3
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-07 17:58:08 -0300

    Reactpipelines

--- vars/reactAwsS3DeployStages.groovy
+++ vars/reactAwsS3DeployStages.groovy
@@ -1,4 +1,9 @@
-def call(Map config) {
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
     stage('Upload Artifact to S3 Bucket') {
         steps {
