3daa0cb9052571022db1a40bd5b3e0a3da2818b7
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-09 15:45:47 -0300

    Corrección de texto

--- vars/awsS3Upload.groovy
+++ vars/awsS3Upload.groovy
@@ -2,22 +2,22 @@ def upload(def artifact, def s3Artifact, def credentials, def region) {
     input "Upload Artifact ${artifact} to S3 Bucket ${s3Artifact}?"
     println artifact
     println s3Artifact
-    withAWS(credentials: "${credentials.awsCredentials}", region: "${credentials.awsRegion}") {
+    withAWS(credentials: "${credentials}", region: "${region}") {
         s3Upload(file:"${artifact}", bucket:"${s3Artifact}", path:"${artifact}")
     }
 }
 
 def uploadFolder(def folder, def s3Artifact, def credentials, def region) {
-    input "Deploy Artifact ${config.artifact} to S3 Bucket ${config.s3Artifact}?"
+    input "Deploy Artifact folder ${folder} to S3 Bucket ${s3Artifact}?"
     println artifact
     println s3Artifact
-    withAWS(credentials: "${config.awsCredentials}", region: "${config.awsRegion}") {
+    withAWS(credentials: "${credentials}", region: "${region}") {
         dir("${folder}") {
             script {
                 files = findFiles(glob: '**')
                 files.each { 
                     println "File:  ${it}"
-                    s3Upload(file:"${it}", bucket:"${config.s3Artifact}", path:"${it}")
+                    s3Upload(file:"${it}", bucket:"${s3Artifact}", path:"${it}")
                 }
             }
         }
