ffe9c2349ec48d39f56eb9386effe8551a4afa30
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-12 12:14:52 -0300

    Refactorización

--- vars/reactAwsS3DeployStages.groovy
+++ vars/reactAwsS3DeployStages.groovy
@@ -1,45 +1,42 @@
 #!/usr/bin/env groovy
+def awsCredentials = [[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-credentials-jenkins']]
+pipeline {
+    agent any
+    
+    options {
+        timeout(time: 1, unit: 'HOURS')
+        disableConcurrentBuilds()
+        parallelsAlwaysFailFast()
+        timestamps()
+    }
 
-def call(config) {
-
-    def awsCredentials = [[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-credentials-jenkins']]
-    pipeline {
-        agent any
-        
-        options {
-            timeout(time: 1, unit: 'HOURS')
-            disableConcurrentBuilds()
-            parallelsAlwaysFailFast()
-            timestamps()
-        }
-
-        parameters {
-            string(defaultValue: 'build', description: 'Static website folder', name: 'BUILD_FOLDER')
-            string(defaultValue: '', description: 'Website S3 Bucket.', name: 'S3_BUCKET')
-            string(defaultValue: 'us-east-1', description: 'AWS region for the pipeline.', name: 'AWS_REGION')
-            string(defaultValue: awsCredentials, description: 'AWS credentials.', name: 'AWS_CREDENTIALS')
-        }
-        stages{
-            stage('Deploy Artifact to S3 Bucket') {
-                    steps {
-                        script{
-                            awsS3Upload.uploadFolder(params.BUILD_FOLDER, params.S3_BUCKET, params.AWS_CREDENTIALS, params.AWS_REGION)
-                        }
-                    }
-                }
-            stage('Check Application is Up and Running') {
+    parameters {
+        string(defaultValue: 'build', description: 'Static website folder', name: 'BUILD_FOLDER')
+        string(defaultValue: '', description: 'Website S3 Bucket.', name: 'S3_BUCKET')
+        string(defaultValue: 'us-east-1', description: 'AWS region for the pipeline.', name: 'AWS_REGION')
+        string(defaultValue: awsCredentials, description: 'AWS credentials.', name: 'AWS_CREDENTIALS')
+    }
+    stages{
+        stage('Deploy Artifact to S3 Bucket') {
                 steps {
-                    echo "Check webpage is deployed"
-                    timeout(300) {
-                        waitUntil {
-                            script {
-                                def r = sh script: "curl -s http://${params.S3_BUCKET}.s3-website-${params.AWS_REGION}.amazonaws.com/", returnStatus: true
-                                return (r == 0);
-                            }
+                    script{
+                        awsS3Upload.uploadFolder(params.BUILD_FOLDER, params.S3_BUCKET, params.AWS_CREDENTIALS, params.AWS_REGION)
+                    }
+                }
+            }
+        stage('Check Application is Up and Running') {
+            steps {
+                echo "Check webpage is deployed"
+                timeout(300) {
+                    waitUntil {
+                        script {
+                            def r = sh script: "curl -s http://${params.S3_BUCKET}.s3-website-${params.AWS_REGION}.amazonaws.com/", returnStatus: true
+                            return (r == 0);
                         }
                     }
                 }
-            } 
-        }
+            }
+        } 
     }
 }
+
