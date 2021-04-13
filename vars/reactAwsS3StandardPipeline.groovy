16ecf35f58599cd7d531991e480237907ffe3cc3
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-13 09:45:46 -0300

    Standard

---
+++ vars/reactAwsS3StandardPipeline.groovy
@@ -0,0 +1,129 @@
+#!/usr/bin/env groovy
+def call(body) {
+
+    //def awsCredentials = [[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-credentials-jenkins']]
+
+    def config = [:]
+    config['GIT_CREDENTIALS'] = 'gitlab-semperti-gonzalo-acosta'
+    config['PIPELINE_LIBRARY_REPOSITORY'] = 'https://gitlab.semperti.com/devops/jenkins-library.git'
+    config['NPM_CMD'] = "npm"
+    config['ENVIRONMENT'] = "development"
+    config['AWS_REGION'] = "us-east-1"
+    config['S3_BUCKET'] = "semperti-react-development-rapientrega"
+    config['BUILD_FOLDER'] = "build"
+    config['SOURCE_FRONTEND'] = "code/frontend"
+    config['AWS_CREDENTIALS'] = 'aws-credentials-jenkins'
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def vGIT_CREDENTIALS = env.GIT_CREDENTIALS == null ? config['GIT_CREDENTIALS'] : env.GIT_CREDENTIALS,
+            vPIPELINE_LIBRARY_REPOSITORY = env.PIPELINE_LIBRARY_REPOSITORY == null ? config['PIPELINE_LIBRARY_REPOSITORY'] : env.PIPELINE_LIBRARY_REPOSITORY,
+            vNPM_CMD = env.NPM_CMD == null ? config['NPM_CMD'] : env.NPM_CMD,
+            vENVIRONMENT = env.ENVIRONMENT == null ? config['ENVIRONMENT'] : env.ENVIRONMENT,
+            vAWS_REGION = env.AWS_REGION == null ? config['AWS_REGION'] : env.AWS_REGION,
+            vS3_BUCKET = env.S3_BUCKET == null ? config['S3_BUCKET'] : env.S3_BUCKET,
+            vBUILD_FOLDER =  env.BUILD_FOLDER == null ? config['BUILD_FOLDER'] : env.BUILD_FOLDER,
+            vSOURCE_FRONTEND = env.SOURCE_FRONTEND == null ? config['SOURCE_FRONTEND'] : env.SOURCE_FRONTEND,
+            vDEPLOY_JOB = env.DEPLOY_JOB == null ? config['DEPLOY_JOB'] : env.DEPLOY_JOB,
+            vAWS_CREDENTIALS_ID = env.AWS_CREDENTIALS == null ? config['AWS_CREDENTIALS'] : env.AWS_CREDENTIALS
+
+    def WS_PATH = ''
+
+    pipeline {
+        agent any
+
+        options {
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+            parallelsAlwaysFailFast()
+            timestamps()
+            //withCredentials(awsCredentials)
+        }
+
+        parameters {
+            credentials( name: 'GIT_CREDENTIALS',  defaultValue: vGIT_CREDENTIALS, description: 'Credenciales para acceder a los repositorios.' )
+            string(name: 'PIPELINE_LIBRARY_REPOSITORY', defaultValue: vPIPELINE_LIBRARY_REPOSITORY, description: 'Repositorio con librerias groovy.')
+            string(name: 'NPM_CMD', defaultValue: vNPM_CMD, description: 'Comando NPM')
+            string(name: 'S3_BUCKET', defaultValue: vS3_BUCKET, description: 'Bucket S3 de publicación')
+            string(name: 'BUILD_FOLDER', defaultValue: vBUILD_FOLDER, description: 'Path relativo a la carpeta de compilación a publicar')
+            string(name: 'AWS_REGION', defaultValue: vAWS_REGION, description: 'AWS Region')
+            string(name: 'DEPLOY_JOB', defaultValue: vDEPLOY_JOB, description: 'Deploy Pipeline Job')
+            string(name: 'SOURCE_FRONTEND', defaultValue: vSOURCE_FRONTEND, description: 'Path relativo del código del frontend')
+            credentials(name: 'AWS_CREDENTIALS',  defaultValue: vAWS_CREDENTIALS_ID, description: 'Credenciales de AWS.' )
+        }
+
+        stages {
+            stage('Checkout Source') {
+                steps {
+                    // git credentialsId: 'dc69dd47-d601-4cb0-adbe-548c17e15506', url: "http://<gitRepo>/<username>/<repoName>.git"
+                    checkout scm
+                    script {
+                        WS_PATH = "${WORKSPACE}"
+                        echo "${WS_PATH}"
+                        dir(params.SOURCE_FRONTEND) {
+                            npmVersion()
+                        }
+                        
+                    }
+                }
+            }
+            stage('Install dependencies') {
+                steps {
+                    dir(params.SOURCE_FRONTEND) {
+                        sh "${params.NPM_CMD} install"
+                    }
+                }
+            }
+            stage('Build React App Dev') {
+                steps {
+                    dir(params.SOURCE_FRONTEND){
+                        sh "${params.NPM_CMD} run build"
+                    }
+                }
+            }
+            stage('Unit Tests') {
+                steps {
+                    dir(params.SOURCE_FRONTEND) {
+                        echo 'Running Unit Tests'
+                        sh "${params.NPM_CMD} test -- --watchAll=false"
+                    }
+                }
+            }
+            stage('Code Analysis') {
+                steps {
+                    script {
+                        dir(params.SOURCE_FRONTEND) {
+                            echo 'Running Code Analysis'
+                        } 
+                    }
+                }
+            }
+            stage('Publish to Nexus') {
+                steps {
+                    echo 'Publish to Nexus'
+                }
+            }
+            stage('Deploy Artifact to S3 Website') {
+                steps {
+                    dir("${params.SOURCE_FRONTEND}"){
+                        input "Deploy Artifact folder ${params.BUILD_FOLDER} to S3 Bucket ${params.S3_BUCKET}?"
+                        withAWS(credentials: "${params.AWS_CREDENTIALS}", region: "${params.AWS_REGION}") {
+                            dir("${params.BUILD_FOLDER}") {
+                                script {
+                                    files = findFiles(glob: '**')
+                                    files.each { 
+                                        println "File:  ${it}"
+                                        s3Upload(file:"${it}", bucket:"${params.S3_BUCKET}", path:"${it}")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
